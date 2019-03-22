ddf2099213d2b8e4b1f5e3ad0c66ab33be471649
Kidd75201 <jbezal@gmail.com>
2019-03-21 23:16:37 -0500

    Completed sidebar with scroll feature, App completed

--- project-5-venue/src/App.js
+++ project-5-venue/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./resources/styles.css";
+import { Element } from "react-scroll";
 
 import Header from "./components/header_footer/Header";
 import Featured from "./components/featured";
@@ -17,11 +18,22 @@ class App extends Component {
         style={{ height: "1500px", background: "cornflowerblue" }}
       >
         <Header />
-        <Featured />
-        <VenueInfo />
-        <Highlight />
-        <Pricing />
-        <Location />
+
+        <Element name="featured">
+          <Featured />
+        </Element>
+        <Element name="venueinfo">
+          <VenueInfo />
+        </Element>
+        <Element name="highlights">
+          <Highlight />
+        </Element>
+        <Element name="pricing">
+          <Pricing />
+        </Element>
+        <Element name="location">
+          <Location />
+        </Element>
         <Footer />
       </div>
     );
